183a3946e996db4c734d42b1a906dfe92afda928
Alex Marmor <aamarmor@gmail.com>
2017-11-20 14:40:28 -0500

    Create user sign up url and button

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def Contact
   end
+
+  def Signup
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#Home'
-
   get '/help', to: 'static_pages#Help'
-
   get '/about', to: 'static_pages#About'
-
   get '/contact', to: 'static_pages#Contact'
+  get '/signup', to: 'users#new'
 end
